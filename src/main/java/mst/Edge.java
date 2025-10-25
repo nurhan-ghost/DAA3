1026af2a6f3f194a453f3436caec6ddfa92d19b3
Nurhan <turganbeknurhan13@gmail.com>
2025-10-25 18:10:07 +0500

    feat(core): implement base graph data structures

---
+++ src/main/java/mst/Edge.java
@@ -0,0 +1,24 @@
+package mst;
+
+public class Edge implements Comparable<Edge> {
+    String from;
+    String to;
+    int weight;
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+
+    @Override
+    public String toString() {
+        return from + " - " + to + " (" + weight + ")";
+    }
+}
+
